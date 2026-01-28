f77c6f1187578f1e3ae7696692a51246cb57be0c
ibrahimamam1 <ibrahima59@iut-dhaka.edu>
2026-01-28 14:59:05 +0600

    deepcopy bug fix

--- src/test/simul.py
+++ src/test/simul.py
@@ -2,6 +2,7 @@ import os
 import sys
 import random
 
+from non_rl_test_env import TestEnv
 from flow.core.params import InFlows
 from flow.core.params import VehicleParams
 from flow.core.params import NetParams
@@ -14,9 +15,10 @@ results_dir = os.path.join(root_dir, "results")
 net_file_dir = os.path.join(root_dir, "networks")
     
 
-def run_simulation(scenario, traffic_rate, intentions, initial_config, env_params, sim_params):
-    expTag = scenario[0]
-    net_file_name = scenario[1]
+def run_simulation(scenario, net_file_name, traffic_rate, intentions, initial_config, env_params, sim_params, car_following_params):
+    print(f'net_file = {net_file_name}, intention={intentions}')
+
+    expTag = scenario
     net_file_path = os.path.join(net_file_dir, net_file_name)
 
     max_vehicle_count_in_inflow = 20
@@ -26,51 +28,43 @@ def run_simulation(scenario, traffic_rate, intentions, initial_config, env_param
     vehicles.add(
             veh_id="NonRL",
             acceleration_controller=(IDMController, {}),
-            car_following_params=SumoCarFollowingParams(
-                min_gap=min_gap,
-                max_speed=max_speed,
-                speed_mode=speed_mode,
-                accel=max_accel,
-                decel=max_decel,
-                sigma=sigma,
-                tau=tau,
-            ),
+            car_following_params= car_following_params,
             num_vehicles=0
         )
     inflow = InFlows()
     inflow.add(
             veh_type="NonRL",
             edge="E#T-X",
-            probability= traffic_rate["N"] / 3600.0,
+            probability= 800 / 3600.0,
             depart_lane="free",
-            depart_speed=initial_speed,
+            depart_speed=5,
             begin=1,
             end=3600,
         )
     inflow.add(
             veh_type="NonRL",
             edge="E#R-X",
-            probability= traffic_rate["E"] / 3600.0,
+            probability= 800 / 3600.0,
             depart_lane="free",
-            depart_speed=initial_speed,
+            depart_speed=5,
             begin=1,
             end=3600,
         )
     inflow.add(
             veh_type="NonRL",
             edge="E#D-X",
-            probability= traffic_rate["S"] / 3600.0,
+            probability= 800 / 3600.0,
             depart_lane="free",
-            depart_speed=initial_speed,
+            depart_speed=5,
             begin=1,
             end=3600,
         )
     inflow.add(
             veh_type="NonRL",
             edge="E#L-X",
-            probability= traffic_rate["W"] / 3600.0,
+            probability= 800 / 3600.0,
             depart_lane="free",
-            depart_speed=initial_speed,
+            depart_speed=5,
             begin=1,
             end=3600,
         )
@@ -78,10 +72,9 @@ def run_simulation(scenario, traffic_rate, intentions, initial_config, env_param
     net_params = NetParams(
             inflows=inflow,
             osm_path=None,
-            template=net_file,
+            template=net_file_path,
         )
 
-from non_rl_test_env import TestEnv
     flow_params = dict(
             exp_tag=expTag,
             env_name=TestEnv,
